bfe5e89e64b811152b4dcf56869d60e827fd018f
rohit <rohit.inea@gmail.com>
2022-01-31 13:57:28 +0530

    init

---
+++ src/firebase/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCvyRVkWE6E1Q4xXCVos9_c6QefKvjUkII",
+  authDomain: "ucracklandingpage.firebaseapp.com",
+  projectId: "ucracklandingpage",
+  storageBucket: "ucracklandingpage.appspot.com",
+  messagingSenderId: "437028500981",
+  appId: "1:437028500981:web:58eb8dc1cb2c71e5f45cbc"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
